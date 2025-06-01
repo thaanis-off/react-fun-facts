c3ac0abc9d52b38dbad2504736fb89b0866415fc
mohamed_thaanis-amuryx <mohamed.thaanis@amuryx.com>
2025-06-01 21:38:10 +0530

    initialization of learn-react project

--- README.md
+++
@@ -1,1 +0,0 @@
-# react-fun-facts
---
+++ components/Navbar.jsx
@@ -0,0 +1,10 @@
+export default function Navbar() {
+    return (
+        <header>
+            <nav>
+                <img src="../assets/react.svg" alt="React logo" />
+                <span>ReactFacts</span>
+            </nav>
+        </header>
+    )
+}
